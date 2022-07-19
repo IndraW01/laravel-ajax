f4e7b8116139f610a4779958693801d4f51a7333
IndraW01 <indraakmal08@gmail.com>
2022-07-19 21:37:55 +0800

    Create Project laravel-ajax

---
+++ app/View/Components/AppLayout.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class AppLayout extends Component
+{
+    public string $title;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(string $title)
+    {
+        $this->title = $title ?? config('app.name');
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('layouts.main');
+    }
+}
